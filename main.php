60e1f3b003696307284b022dcbc3899cdcdbc622
Nicole Furlan <nicolempitts@gmail.com>
2021-03-18 21:35:58 -0400

    Initial checkin

---
+++ main.php
@@ -0,0 +1,137 @@
+<section>
+	<h2>Skills</h2>
+	<ul class="skills">
+		<li>HTML</li>
+		<li>CSS/Sass</li>
+		<li>PHP</li>
+		<li>JavaScript (ES6+)</li>
+		<li>jQuery</li>
+		<li>Git</li>
+		<li>Linux</li>
+		<li>Apache</li>
+		<li>Shell</li>
+		<li>MySQL</li>
+		<li>phpMyAdmin</li>
+		<li>APIs</li>
+		<li>Wordpress</li>
+		<li>CMS Development</li>
+		<li>Static/JAMstack Websites</li>
+		<li>Progressive Web Apps</li>
+		<li>Google Marketing Platform</li>
+		<li>Google Search Console</li>
+		<li>Google Analytics</li>
+		<li>Google Tag Manager</li>
+		<li>Google AdSense</li>
+		<li>Google Optimize</li>
+		<li>Adobe Creative Cloud</li>
+	</ul>
+</section>
+<section>
+	<h2>Projects</h2>
+	<ul>
+		<li><a href="https://dogsafefoods.com" target="_blank" rel="noopener"><span role="img" title="Dog">&#x1F415;</span>&nbsp;Dog&nbsp;Safe&nbsp;Foods</a></li>
+		<li><a href="https://catsafefoods.com" target="_blank" rel="noopener"><span role="img" title="Cat">&#x1F408;</span>&nbsp;Cat&nbsp;Safe&nbsp;Foods</a></li>
+		<li><a href="https://simplepwa.com" target="_blank" rel="noopener"><span role="img" title="Technologist">&#x1F469;&#x200D;&#x1F4BB;</span>&nbsp;Simple&nbsp;PWA</a></li>
+		<li><a href="https://dev.nicolefurlan.com" target="_blank" rel="noopener"><span role="img" title="Email">&#x1F4E7;</span>&nbsp;Gmail&nbsp; Analyzed</a> (in progress)</li>
+	</ul>
+</section>
+<section>
+	<h2>Education</h2>
+	<ul>
+		<li>
+			<span class="bold">Master of Software Engineering <a href="https://psu.edu" target="_blank" rel="noopener">Penn State University</a></span> <span class="italic">(remote, 2011-2012)</span>
+			<ul>
+				<li><a href="https://www.worldcampus.psu.edu/degrees-and-certificates/software-engineering-masters/courses" target="_blank" rel="noopener">Coursework</a> focused heavily on each phase of the software development life cycle</li>
+			</ul>
+		</li>
+		<li>
+			<span class="bold">Bachelor of Science in Computer Science <a href="https://umassd.edu" target="_blank" rel="noopener">University of Massachusetts</a></span> <span class="italic">(Dartmouth MA, 2007-2010)</span>
+			<ul>
+				<li><a href="https://www.umassd.edu/engineering/cis/undergraduate/curriculum-and-courses/" target="_blank" rel="noopener">Coursework</a> built a strong foundation of computer science with programming in C and Java</li>
+			</ul>
+		</li>
+	</ul>
+</section>
+<section>
+	<h2>Experience</h2>
+	<ul class="experience">
+		<li>
+			<a href="https://mercyforanimals.org" target="_blank" rel="noopener">Mercy for Animals</a> <span class="italic">(remote, 2015-2019)</span>
+			<p class="bold">Summary:</p>
+			<ul>
+				<li>Created, maintained, and fixed issues with websites and web applications, including a large existing codebase with minimal documentation</li>
+				<li>Hired and managed a team of three web developers</li>
+				<li>Wrote and reviewed a large amount of technical and non-technical documentation</li>
+			</ul>
+			<details>
+				<summary><span class="bold">Full Stack Web Developer</span>&nbsp;<span class="italic">(2016-2018)</span>, <span class="bold">Associate Director of Web Development</span>&nbsp;<span class="italic">(2018-2019)</span></summary>
+				<ul>
+					<li>Communicated with other departments and developed software solutions to address their technical needs</li>
+					<li>Advised and guided the organization on higher-level technical functionality and planning</li>
+					<li>Assisted with creating, updating, and fixing issues with websites and web applications written in HTML, CSS, Javascript, PHP, and MySQL including a large existing codebase with minimal documentation</li>
+					<li>Assisted in creating and maintaining a large library of technical documentation for both technical and non-technical staff members</li>
+					<li>Used Git for code version control and managed large code base with multiple users</li>
+					<li>Built team of three developers, including analyzing team needs, creating job postings, interviewing candidates, and selecting new hires</li>
+					<li>Managed and mentored team members</li>
+					<li>Managed projects, eliminated roadblocks, delegated tasks according to team member suitability and workload</li>
+					<li>Conducted code reviews to ensure functionality and quality</li>
+					<li>Kept on top of new trends for websites and web applications</li>
+				</ul>
+			</details>
+			<details>
+				<summary><span class="bold">Web Developer (2015-2016)</span></summary>
+				<ul>
+					<li>Created, updated, and fixed issues with websites and applications written in HTML, CSS, Javascript, PHP, and MySQL</li>
+					<li>Used Git for code version control</li>
+					<li>Created technical documentation for both technical and non-technical staff members</li>
+					<li>Kept on top of new trends for websites and web applications</li>
+				</ul>
+			</details>
+		</li>
+		<li>
+			<a href="https://animaloutlook.org" target="_blank" rel="noopener">Animal Outlook</a>&nbsp;<span class="italic">(remote, 2013-2015)</span>
+			<p class="bold">Summary:</p>
+			<ul>
+				<li>Created, maintained, and fixed issues with websites and web applications</li>
+				<li>Built and maintained a LAMP server</li>
+				<li>Wrote a large amount of technical and non-technical documentation</li>
+			</ul> 
+			<details>
+				<summary><span class="bold">Web Developer</span>&nbsp;<span class="italic">(2013)</span>, <span class="bold">Director of Technology</span>&nbsp;<span class="italic">(2013-2015)</span></summary>
+				<ul>
+					<li>Developed and maintained WordPress themes, plugins, and 3rd party integrations in HTML, CSS, Javascript, PHP, and MySQL</li>
+					<li>Migrated several websites to Wordpress with the goal of non-technical staff maintainability</li>
+					<li>Built and maintained a secure and reliable Linux server with Apache, MySQL, and PHP (LAMP), which hosted a large number of websites</li>
+					<li>Set up and used Git for code version control</li>
+					<li>Developed graphics using Adobe Photoshop upon request</li>
+					<li>Reviewed and edited web content and layout</li>
+					<li>Developed and maintained documentation with the level of user technical expertise in mind</li>
+					<li>Coordinated and/or assisted with other projects as determined</li>
+					<li>Designed and sent out newsletters using Mailchimp</li>
+					<li>Set up tracking on websites including Google Search Console, Google Analytics, and Google Adwords</li>
+					<li>Contributed to strategies for driving traffic to websites, including writing blog articles as time allows</li>
+					<li>Continuously searched for ways to improve websites</li>
+				</ul>
+			</details>
+		</li>
+		<li>
+			<a href="https://gdmissionsystems.com" target="_blank" rel="noopener">General Dynamics Mission Systems</a>&nbsp;<span class="italic">(Taunton MA, 2008-2013)</span>
+			<p class="bold">Summary:</p>
+			<ul>
+				<li>Wrote, tested, and fixed embedded software</li>
+				<li>Managed the software release process for our engineering team</li>
+				<li>Wrote and reviewed technical documentation</li>
+			</ul>
+			<details>
+				<summary><span class="bold">Software Engineer</span>&nbsp;<span class="italic">(2010-2013)</span>, <span class="bold">Software Engineering Intern</span>&nbsp;<span class="italic">(2008-2010)</span></summary>
+				<ul>
+					<li>Wrote software for embedded systems in C and Java</li>
+					<li>Tested software and fixed bugs in C and Java</li>
+					<li>Worked in collaboration with other teams and departments in a large organization</li>
+					<li>Managed the entire software release process for a large team of engineers</li>
+					<li>Wrote and reviewed documentation for other technical users</li>
+				</ul>
+			</details>
+		</li>
+	</ul>
+</section>
